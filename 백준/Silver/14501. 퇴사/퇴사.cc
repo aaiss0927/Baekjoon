4b655c33597fb00df296198758a1d8de095a13b4
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2023-08-07 00:13:15 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -2,75 +2,30 @@
 #include <vector>
 using namespace std;
 
-int N;
-int cnt = 0;
-vector<int> arr;
-vector<vector<int>> case_able;
-
-void search(int k, vector<pair<int, int>>& v) {
-	if (k == N) {
-		cnt++;
-		vector<bool> isUsed(N, false);
-		bool flag = true;
-		for (int i = 0; i < arr.size(); i++) {
-			for (int j = 0; j < v[arr[i] - 1].first; j++) {
-				if (arr[i] - 1 + j >= N) {
-					flag = false;
-					break;
-				}
-				if (isUsed[arr[i] - 1 + j]) {
-					flag = false;
-					break;
-				}
-				else {
-					isUsed[arr[i] - 1 + j] = true;
-				}
-			}
-			if (!flag) {
-				break;
-			}
-		}
-		if (flag) {
-			vector<int> temp;
-			for (int i = 0; i < arr.size(); i++) {
-				temp.push_back(arr[i]);
-			}
-			case_able.push_back(temp);
-		}
-		return;
-	}
-	else {
-		arr.push_back(k + 1);
-		search(k + 1, v);
-		arr.pop_back();
-		search(k + 1, v);
-	}
-}
+vector<int> dp(16, 0);
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
+	int N; cin >> N;
 	vector<pair<int, int>> v;
-	for (int i = 0; i < N; i++) {
+	v.emplace_back(0, 0);
+	for (int i = 1; i <= N; i++) {
 		int t, p;
 		cin >> t >> p;
 		v.emplace_back(t, p);
 	}
 
-	search(0, v);
-
-	int max = 0;
-	for (int i = 0; i < case_able.size(); i++) {
-		int result = 0;
-		for (int j = 0; j < case_able[i].size(); j++) {
-			result += v[case_able[i][j] - 1].second;
+	for (int i = N; i >= 1; i--) {
+		if (i + v[i].first > N + 1) {
+			dp[i] = dp[i + 1];
 		}
-		if (result > max) {
-			max = result;
+		else {
+			dp[i] = max(dp[i + 1], v[i].second + dp[i + v[i].first]);
 		}
 	}
-	cout << max;
+
+	cout << dp[1];
 }
