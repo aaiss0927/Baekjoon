93eae0e4b591b69dc6496194e6eadf85feaf22b2
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-29 00:15:12 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,65 +1,76 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int N;
-int dx[4] = { 0, 1, 0, -1 };
-int dy[4] = { 1, 0, -1, 0 };
+int n;
+int grid[100][100];
+bool visited[100][100];
+int max_height = INT_MIN;
+int ans = INT_MIN;
+int cnt;
 
-void dfs(pair<int, int> p, int h, vector<vector<int>>& board, vector<vector<bool>>& visited) {
-    visited[p.first][p.second] = true;
-    for (int i = 0; i < 4; i++) {
-        int x = p.first + dx[i];
-        int y = p.second + dy[i];
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
 
-        if (x < 0 || x >= N || y < 0 || y >= N) {
-            continue;
-        }
+bool in_range(int x, int y) {
+	return (x >= 0 && x < n && y >= 0 && y < n);
+}
 
-        if (visited[x][y]) {
-            continue;
-        }
+bool can_go(int x, int y, int danger) {
+	return (in_range(x, y) && !visited[x][y] && grid[x][y] > danger);
+}
 
-        if (board[x][y] <= h) {
-            continue;
-        }
+void DFS(int x, int y, int danger) {
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
 
-        dfs({ x, y }, h, board, visited);
-    }
+		if (can_go(nx, ny, danger)) {
+			visited[nx][ny] = true;
+			DFS(nx, ny, danger);
+		}
+	}
+}
+
+void setup() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			visited[i][j] = false;
+		}
+	}
+
+	cnt = 0;
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	ios::sync_with_stdio(0);
 
-    cin >> N;
-    vector<vector<int>> board(N, vector<int>(N));
-    int max = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cin >> board[i][j];
-            if (board[i][j] > max) {
-                max = board[i][j];
-            }
-        }
-    }
+	cin >> n;
 
-    int ans = 0;
-    for (int i = 0; i < max; i++) {
-        vector<vector<bool>> visited(N, vector<bool>(N, false));
-        int cnt = 0;
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-                if (!visited[j][k] && (board[j][k] > i)) {
-                    dfs({ j, k }, i, board, visited);
-                    cnt++;
-                }
-            }   
-        }
-        if (cnt > ans) {
-            ans = cnt;
-        }
-    }
-    cout << ans;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+			max_height = max(max_height, grid[i][j]);
+		}
+	}
+
+	for (int danger = 0; danger < max_height; danger++) {
+		setup();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j] && grid[i][j] > danger) {
+					visited[i][j] = true;
+					DFS(i, j, danger);
+					cnt++;
+				}
+			}
+		}
+
+		ans = max(ans, cnt);
+	}
+	
+	cout << ans;
 }
