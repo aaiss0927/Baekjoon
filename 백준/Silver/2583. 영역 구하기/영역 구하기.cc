99b04c6584badc01711d637ea8bc8bea9abb6946
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-02-29 00:33:04 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,76 +1,89 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int M, N, K;
-vector<vector<int>> board;
-vector<vector<bool>> visited;
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
-int cnt;
+int m, n, k;
+int grid[100][100];
+bool visited[100][100];
+int cnt_domain;
+vector<int> areas;
+int area;
 
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, -1, 0, 1 };
 
-void dfs(pair<int ,int> p) {
-	visited[p.first][p.second] = true;
-	cnt++;
+bool in_range(int x, int y) {
+	return (x >= 0 && x < m && y >= 0 && y < n);
+}
 
+bool can_go(int x, int y) {
+	return (in_range(x, y) && !visited[x][y] && grid[x][y] == 0);
+}
+
+void DFS(int x, int y) {
 	for (int i = 0; i < 4; i++) {
-		int x = p.second + dx[i];
-		int y = p.first + dy[i];
+		int nx = x + dx[i];
+		int ny = y + dy[i];
 
-		if (x < 0 || x >= N || y < 0 || y >= M) {
-			continue;
+		if (can_go(nx, ny)) {
+			visited[nx][ny] = true;
+			area++;
+			DFS(nx, ny);
 		}
-
-		if (visited[y][x]) {
-			continue;
-		}
-
-		if (board[y][x] == 0) {
-			continue;
-		}
-
-		dfs({ y, x });
 	}
 }
 
+void setup() {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			visited[i][j] = false;
+		}
+	}
+
+	cnt_domain = 0;
+}
+
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(0);
+	ios::sync_with_stdio(0);
 
-	cin >> M >> N >> K;
+	cin >> m >> n >> k;
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			grid[i][j] = 0;
+		}
+	}
+
+	while (k--) {
+		int c1, r1, c2, r2; cin >> c1 >> r1 >> c2 >> r2;
+
+		for (int i = r1; i < r2; i++) {
+			for (int j = c1; j < c2; j++) {
+				grid[i][j] = 1;
+			}
+		}
+	}
+
+	setup();
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (can_go(i, j)) {
+				visited[i][j] = true;
+				area = 1;
+				DFS(i, j);
+				cnt_domain++;
+				areas.push_back(area);
+			}
+		}
+	}
+
+	sort(areas.begin(), areas.end());
 	
-	board = vector<vector<int>>(M, vector<int>(N, 1));
-	visited = vector<vector<bool>>(M, vector<bool>(N, false));
-
-	for (int i = 0; i < K; i++) {
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-
-		for (int j = M - y2; j <= M - y1 - 1; j++) {
-			for (int k = x1; k <= x2 - 1; k++) {
-				board[j][k] = 0;
-			}
-		}
-	}
-
-	vector<int> result;
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			if (board[i][j] == 1 && !visited[i][j]) {
-				cnt = 0;
-				dfs({ i, j });
-				result.push_back(cnt);
-			}
-		}
-	}
-	sort(result.begin(), result.end());
-
-	cout << result.size() << '\n';
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << " ";
+	cout << cnt_domain << '\n';
+	for (auto v : areas) {
+		cout << v << ' ';
 	}
 }
