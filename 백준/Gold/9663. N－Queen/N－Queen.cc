49a6a795dfcf3adda5c7f4be009d13e7540b975f
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-12 09:33:05 +0900

    [Gold IV] Title: N-Queen, Time: 2492 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,59 +1,51 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 using namespace std;
 
-int N;
-int cnt = 0;
-vector<bool> check_col;
-vector<bool> check_lr;
-vector<bool> check_rl;
+int n;
+int grid[14][14];
+int arr[14];
+bool visited[14];
+int cnt;
 
-bool check(int row, int col) {
-	if (check_col[col]) {
-		return false;
+bool check(int k) {
+	int add = k + arr[k];
+	int sub = k - arr[k];
+
+	for (int i = 0; i < k; i++) {
+		if (i + arr[i] == add || i - arr[i] == sub) {
+			return false;
+		}
 	}
-	if (check_lr[row - col + N - 1]) {
-		return false;
-	}
-	if (check_rl[row + col]) {
-		return false;
-	}
+
 	return true;
 }
 
-void process(int row) {
-	if (row == N) {
+void bt(int k) {
+	if (k == n) {
 		cnt++;
 		return;
 	}
-	for (int i = 0; i < N; i++) {
-		if (!check(row, i)) {
-			continue;
-		}
-		else {
-			check_col[i] = true;
-			check_lr[row - i + N - 1] = true;
-			check_rl[row + i] = true;
-			process(row + 1);
-			check_col[i] = false;
-			check_lr[row - i + N - 1] = false;
-			check_rl[row + i] = false;
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			arr[k] = i;
+			grid[k][arr[k]] = 1;
+
+			if (!check(k)) {
+				visited[i] = false;
+				grid[k][arr[k]] = 0;
+				continue;
+			}
+
+			bt(k + 1);
+			visited[i] = false;
 		}
 	}
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N;
-	check_col = vector<bool>(N, false);
-	check_lr = vector<bool>(2 * N - 1, false);
-	check_rl = vector<bool>(2 * N - 1, false);
-
-	process(0);
-
+	cin >> n;
+	bt(0);
 	cout << cnt;
 }
