470f26d74e13f60a7564b2b01268c8439c8ae65d
aaiss0927 <101855229+aaiss0927@users.noreply.github.com>
2024-04-21 19:49:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -9,20 +9,8 @@ int main() {
 	int a, b, c, d, e, f; cin >> a >> b >> c >> d >> e >> f;
 	int x, y;
 
-	if (e == 0) {
-		x = f / d;
-		y = ((c * d) - (f * a)) / (b * d);
-	}
-
-	else if (d == 0) {
-		y = f / e;
-		x = ((c * e) - (f * b)) / (a * e);
-	}
-
-	else {
-		x = ((c * e) - (b * f)) / ((a * e) - (b * d));
-		y = ((c * d) - (a * f)) / ((b * d) - (e * a));
-	}
+	x = ((c * e) - (b * f)) / ((a * e) - (b * d));
+	y = ((c * d) - (a * f)) / ((b * d) - (e * a));
 
 	cout << x << ' ' << y;
 }
