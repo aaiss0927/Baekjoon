10787bfffb6c6d54cd540d50f9547543edfdf4a0
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-08-20 18:51:49 +0900

    [Silver III] Title: 1로 만들기, Time: 24 ms, Memory: 6868 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,41 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-vector<int> dp(10000001);
-int cnt;
+int n;
+vector<bool> visited;
+queue<int> q;
+vector<int> s_p;
 
-int op(int x) {
-	for (int i = 2; i <= x; i++) {
-		if (i % 6 == 0) {
-			dp[i] = min(dp[i / 3], dp[i / 2]) + 1;
+void bfs() {
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+
+		if (s_p[n] != -1)
+			return;
+
+		if (!(cur + 1 > n) && !visited[cur + 1]) {
+			visited[cur + 1] = true;
+			s_p[cur + 1] = s_p[cur] + 1;
+			q.push(cur + 1);
 		}
-		else if (i % 6 == 1) {
-			dp[i] = dp[i - 1] + 1;
+		
+		if (!(cur * 2 > n) && !visited[cur * 2]) {
+			visited[cur * 2] = true;
+			s_p[cur * 2] = s_p[cur] + 1;
+			q.push(cur * 2);
 		}
-		else if (i % 6 == 2) {
-			dp[i] = min(dp[i / 2], dp[i - 1]) + 1;
-		}
-		else if (i % 6 == 3) {
-			dp[i] = min(dp[i / 3], dp[i - 1]) + 1;
-		}
-		else if (i % 6 == 4) {
-			dp[i] = min(dp[i / 2], dp[i - 1]) + 1;
-		}
-		else if (i % 6 == 5) {
-			dp[i] = dp[i - 1] + 1;
+			
+
+		if (!(cur * 3 > n) && !visited[cur * 3]) {
+			visited[cur * 3] = true;
+			s_p[cur * 3] = s_p[cur] + 1;
+			q.push(cur * 3);
 		}
 	}
-	return dp[x];
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin >> n;
+	visited = vector<bool>(n + 1, false);
+	s_p = vector<int>(n + 1, -1);
 
-	int x; cin >> x;
-	dp[1] = 0;
-	cout << op(x);
+	q.push(1);
+	visited[1] = true;
+	s_p[1] = 0;
+	bfs();
+	cout << s_p[n];
 }
