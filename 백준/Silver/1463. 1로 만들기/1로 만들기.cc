27a4334f89eddd7f68d11174c6c5a0feb50ea765
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-08-20 19:20:14 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,49 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
-int n;
-vector<bool> visited;
-queue<int> q;
-vector<int> s_p;
+int main() {
+	int n; cin >> n;
+	vector<int> dp(n + 1, -1);
+	dp[1] = 0;
 
-void bfs() {
-	while (!q.empty()) {
-		int cur = q.front(); q.pop();
-
-		if (s_p[n] != -1)
-			return;
-
-		if (!(cur + 1 > n) && !visited[cur + 1]) {
-			visited[cur + 1] = true;
-			s_p[cur + 1] = s_p[cur] + 1;
-			q.push(cur + 1);
+	for (int i = 2; i <= n; i++) {
+		if (i % 2 == 0 && i % 3 == 0) {
+			dp[i] = min(min(dp[i - 1], dp[i / 2]), dp[i / 3]) + 1;
 		}
-		
-		if (!(cur * 2 > n) && !visited[cur * 2]) {
-			visited[cur * 2] = true;
-			s_p[cur * 2] = s_p[cur] + 1;
-			q.push(cur * 2);
-		}
-			
 
-		if (!(cur * 3 > n) && !visited[cur * 3]) {
-			visited[cur * 3] = true;
-			s_p[cur * 3] = s_p[cur] + 1;
-			q.push(cur * 3);
+		else if (i % 2 == 0 && i % 3 != 0) {
+			dp[i] = min(dp[i - 1], dp[i / 2]) + 1;
+		}
+
+		else if (i % 2 != 0 && i % 3 == 0) {
+			dp[i] = min(dp[i - 1], dp[i / 3]) + 1;
+		}
+
+		else {
+			dp[i] = dp[i - 1] + 1;
 		}
 	}
-}
 
-int main() {
-	cin >> n;
-	visited = vector<bool>(n + 1, false);
-	s_p = vector<int>(n + 1, -1);
-
-	q.push(1);
-	visited[1] = true;
-	s_p[1] = 0;
-	bfs();
-	cout << s_p[n];
+	cout << dp[n];
 }
