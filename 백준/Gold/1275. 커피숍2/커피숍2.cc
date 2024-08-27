e83f9a81e5b10ac50dd31fa00462fd1cc0b58b9e
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-08-28 01:12:44 +0900

    [Gold I] Title: 커피숍2, Time: 112 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long long> v;
+vector<long long> t;
+
+void init(int node, int s, int e) {
+	if (s == e)
+		t[node] = v[s];
+
+	else {
+		init(node * 2, s, (s + e) / 2);
+		init(node * 2 + 1, (s + e) / 2 + 1, e);
+		t[node] = t[node * 2] + t[node * 2 + 1];
+	}
+}
+
+long long query(int node, int s, int e, int l, int r) {
+	if (r < s || l > e)
+		return 0;
+
+	if (l <= s && r >= e)
+		return t[node];
+
+	long long lsum = query(node * 2, s, (s + e) / 2, l, r);
+	long long rsum = query(node * 2 + 1, (s + e) / 2 + 1, e, l, r);
+
+	return lsum + rsum;
+}
+
+void update(int node, int s, int e, int idx, long long val) {
+	if (idx < s || idx > e)
+		return;
+
+	if (s == e) {
+		v[idx] = val;
+		t[node] = val;
+		return;
+	}
+
+	update(node * 2, s, (s + e) / 2, idx, val);
+	update(node * 2 + 1, (s + e) / 2 + 1, e, idx, val);
+	t[node] = t[node * 2] + t[node * 2 + 1];
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, m; cin >> n >> m;
+
+	v = vector<long long>(n);
+	t = vector<long long>(4 * n);
+
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	init(1, 0, n - 1);
+
+	for (int i = 0; i < m; i++) {
+		long long a, b, c, d; cin >> a >> b >> c >> d;
+
+		if (a > b)
+			swap(a, b);
+
+		cout << query(1, 0, n - 1, a - 1, b - 1) << '\n';
+		update(1, 0, n - 1, c - 1, d);
+	}
+}
