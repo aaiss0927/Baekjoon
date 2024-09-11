90716bf5b24dafdffe93163f42d02cd7b5129705
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-09-11 22:31:31 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string id; cin >> id;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << id << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
