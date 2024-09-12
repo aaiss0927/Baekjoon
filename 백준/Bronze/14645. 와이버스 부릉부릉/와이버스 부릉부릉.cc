526c6e8ac8bb76dee42b38e9cbdab80e140b465a
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-09-12 22:20:46 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, k; cin >> n >> k;
+
+    while (n--) {
+        int a, b; cin >> a >> b;
+    }
+    cout << "비와이";
+}
