4b0e5a73171fdfefa00cd9167b2d3e92a1b568b8
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2024-11-08 23:39:41 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -5,12 +5,8 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-
-	while (1) {
-		string str;
-		if (getline(cin, str))
-			cout << str << '\n';
-		else
-			break;
-	}
+    
+    string str;
+	while (getline(cin, str))
+		cout << str << '\n';
 }
