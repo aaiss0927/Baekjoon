f05c309964f5e04e9f946abc20c89a5f90fac78f
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2025-01-04 14:39:34 +0900

    [Gold V] Title: 두 용액, Time: 16 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n; cin >> n;
+	vector<long long> solution(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> solution[i];
+
+	sort(solution.begin(), solution.end());
+
+	long long first, second;
+	long long sum = LLONG_MAX;
+
+	for (int i = 0; i < n; i++) {
+		auto it = lower_bound(solution.begin(), solution.end(), -solution[i]);
+		int j, k;
+
+		if (it == solution.end()) {
+			j = (--it) - solution.begin();
+
+			if (i != j && abs(solution[i] + solution[j]) < sum) {
+				first = solution[i];
+				second = solution[j];
+				sum = abs(solution[i] + solution[j]);
+			}
+		}
+
+		else {
+			j = it - solution.begin();
+
+			if (j == 0) {
+				if (i != j && abs(solution[i] + solution[j]) < sum) {
+					first = solution[i];
+					second = solution[j];
+					sum = abs(solution[i] + solution[j]);
+				}
+			}
+
+			else {
+				k = j - 1;
+
+				if (i != j && abs(solution[i] + solution[j]) < abs(solution[i] + solution[k])) {					
+					if (abs(solution[i] + solution[j]) < sum) {
+						first = solution[i];
+						second = solution[j];
+						sum = abs(solution[i] + solution[j]);
+					}
+				}
+
+				else if (i != k && abs(solution[i] + solution[j]) >= abs(solution[i] + solution[k])) {
+					if (abs(solution[i] + solution[k]) < sum) {
+						first = solution[i];
+						second = solution[k];
+						sum = abs(solution[i] + solution[k]);
+					}
+				}
+			}
+		}
+	}
+
+	if (first > second)
+		swap(first, second);
+
+	cout << first << ' ' << second;
+}
