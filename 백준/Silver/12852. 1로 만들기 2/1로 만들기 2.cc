e73f5495e57a5240938c55333e25af35df32c601
조현준_T7254 <101855229+aaiss0927@users.noreply.github.com>
2025-01-31 16:38:26 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,74 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int x;
-vector<int> dp;
-vector<int> prev_num;
+int dp[1000001];
+int prev_num[1000001];
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-	cin >> x;
-	dp = vector<int>(x + 1);
-	prev_num = vector<int>(x + 1);
-	dp[1] = 0;
-	prev_num[1] = 0;
+    int x; cin >> x;
+    
+    for (int i = 1; i <= x; i++)
+        dp[i] = INT_MAX;
 
-	for (int i = 2; i <= x; i++) {
-		if (i % 2 == 0 && i % 3 == 0) {
-			if (min(min(dp[i / 2], dp[i / 3]), dp[i - 1]) == dp[i - 1]) {
-				dp[i] = dp[i - 1] + 1;
-				prev_num[i] = i - 1;
-			}
+    dp[x] = 0;
 
-			else if (min(min(dp[i / 2], dp[i / 3]), dp[i - 1]) == dp[i / 2]) {
-				dp[i] = dp[i / 2] + 1;
-				prev_num[i] = i / 2;
-			}
+    for (int i = x; i > 1; i--) {
+        if (i % 3 == 0) {
+            if (dp[i] + 1 < dp[i / 3]) {
+                dp[i / 3] = dp[i] + 1;
+                prev_num[i / 3] = i;
+            }
+        }
 
-			else {
-				dp[i] = dp[i / 3] + 1;
-				prev_num[i] = i / 3;
-			}
-		}
+        if (i % 2 == 0) {
+            if (dp[i] + 1 < dp[i / 2]) {
+                dp[i / 2] = dp[i] + 1;
+                prev_num[i / 2] = i;
+            }
+        }
 
-		else if (i % 2 == 0 && i % 3 != 0) {
-			if (min(dp[i - 1], dp[i / 2]) == dp[i - 1]) {
-				dp[i] = dp[i - 1] + 1;
-				prev_num[i] = i - 1;
-			}
+        if (dp[i] + 1 < dp[i - 1]) {
+            dp[i - 1] = dp[i] + 1;
+            prev_num[i - 1] = i;
+        }
+    }
 
-			else {
-				dp[i] = dp[i / 2] + 1;
-				prev_num[i] = i / 2;
-			}
-		}
+    cout << dp[1] << '\n';
+    int cur = 1;
+    vector<int> v = { 1 };
 
-		else if (i % 2 != 0 && i % 3 == 0) {
-			if (min(dp[i - 1], dp[i / 3]) == dp[i - 1]) {
-				dp[i] = dp[i - 1] + 1;
-				prev_num[i] = i - 1;
-			}
+    while (cur != x) {
+        cur = prev_num[cur];
+        v.push_back(cur);
+    }
 
-			else {
-				dp[i] = dp[i / 3] + 1;
-				prev_num[i] = i / 3;
-			}
-		}
-
-		else {
-			dp[i] = dp[i - 1] + 1;
-			prev_num[i] = i - 1;
-		}
-	}
-
-
-	cout << dp[x] << '\n';
-	while (x) {
-		cout << x << ' ';
-		x = prev_num[x];
-	}
+    for (int i = v.size() - 1; i >= 0; i--)
+        cout << v[i] << ' ';
 }
