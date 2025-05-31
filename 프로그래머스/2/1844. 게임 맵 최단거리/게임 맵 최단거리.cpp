03417055aef38203e21f94f06a9300d9cbe03133
조현준 <101855229+aaiss0927@users.noreply.github.com>
2025-05-31 22:54:20 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.30 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <climits>
 using namespace std;
 
 int n, m;
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
-vector<vector<int>> maps;
+vector<vector<int>> map;
 vector<vector<bool>> visited;
 vector<vector<int>> dist;
 queue<pair<int, int>> q;
 
 bool in_range(int x, int y) {
-    return x >= 0 && x < n && y >= 0 && y < m;
+    return (x >= 0 && x < n && y >= 0 && y < m);
 }
 
 bool can_go(int x, int y) {
-    return in_range(x, y) && !visited[x][y] && maps[x][y];
+    return in_range(x, y) && map[x][y] && !visited[x][y];
 }
 
 void bfs() {
@@ -25,14 +26,14 @@ void bfs() {
         int y = q.front().second;
         q.pop();
         
-        for (int i = 0 ; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
             if (can_go(nx, ny)) {
-                visited[nx][ny] = true;
-                dist[nx][ny] = dist[x][y] + 1;
                 q.push({nx, ny});
+                visited[nx][ny] = true;
+                dist[nx][ny] = min(dist[nx][ny], dist[x][y] + 1);
             }
         }
     }
@@ -40,17 +41,22 @@ void bfs() {
 
 int solution(vector<vector<int>> input_maps) {
     int answer = 0;
-    maps = input_maps;
-    n = maps.size();
-    m = maps[0].size();
-    visited = vector<vector<bool>> (n, vector<bool> (m, false));
-    dist = vector<vector<int>> (n, vector<int> (m, -1));
-    dist[0][0] = 1;
+    
+    n = input_maps.size();
+    m = input_maps[0].size();
+    map = input_maps;
+    visited = vector<vector<bool>>(n, vector<bool>(m, false));
+    dist = vector<vector<int>>(n, vector<int>(m, INT_MAX));
     
     q.push({0, 0});
     visited[0][0] = true;
+    dist[0][0] = 1;
     bfs();
     answer = dist[n - 1][m - 1];
     
+    if (answer == INT_MAX) answer = -1;
+    
     return answer;
 }
+
+
