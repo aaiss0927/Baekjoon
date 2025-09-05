f13f79fdca246f2d20075234b528fda54a8a324e
조현준 <101855229+aaiss0927@users.noreply.github.com>
2025-09-05 15:34:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int grid[50][50];
+// 0 청소x 1 청소o
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+bool is_range(int x, int y) {
+	return (x >= 0 && x < n&& y >= 0 && y < m);
+}
+
+bool is_clear(int x, int y) {
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (!is_range(nx, ny))
+			continue;
+
+		if (grid[nx][ny] == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n >> m;
+	int r, c, d; cin >> r >> c >> d;
+	// d... 0위 1오 2 아 3왼
+	int cnt = 0;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> grid[i][j];
+
+	while (true) {
+		if (grid[r][c] == 0) {
+			grid[r][c] = 2;
+			cnt++;
+		}
+
+		if (is_clear(r, c)) {
+			if (is_range(r - dx[d], c - dy[d]) && grid[r - dx[d]][c - dy[d]] != 1) {
+				r -= dx[d]; c -= dy[d];
+				continue;
+			}
+
+			else
+				break;
+		}
+
+		else {
+			d = (d + 3) % 4;
+			while (grid[r + dx[d]][c + dy[d]] != 0)
+				d = (d + 3) % 4;
+
+			r += dx[d]; c += dy[d];
+		}
+	}
+
+	cout << cnt;
+}
