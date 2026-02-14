dd73f06c18258d5172a13d0e266730b743f16aec
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-02-14 21:51:18 +0900

    [Silver IV] Title: 평행사변형, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.cc
+++ 백준/Silver/1064. 평행사변형/평행사변형.cc
@@ -1,72 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Point {
-private:
-	int x, y;
-public:
-	Point()
-		: x(0), y(0) {}
-	Point(int x_, int y_)
-		: x(x_), y(y_) {}
-	int getX() {
-		return x;
-	}
-	int getY() {
-		return y;
-	}
-	void setX(int x) {
-		this->x = x;
-	}
-	void setY(int y) {
-		this->y = y;
-	}
-};
-
-bool line(Point f, Point s, Point t) {
-	bool condition;
-	if ((((s.getX() - f.getX()) != 0) && (((t.getX() - s.getX())) != 0))) {
-		condition = (double((s.getY() - f.getY())) / (s.getX() - f.getX()) ==
-			double((t.getY() - s.getY())) / (t.getX() - s.getX()));
-	}
-	else if ((((s.getX() - f.getX()) == 0) && (((t.getX() - s.getX())) == 0))) {
-		return true;
-	}
-	else {
-		condition = false;
-	}
-
-	if (condition) {
-		return true;
-	}
-	return false;
-}
-
-double get_length(Point f, Point s) {
-	return sqrt(pow((f.getX() - s.getX()), 2) + pow((f.getY() - s.getY()), 2));
+double l2_norm(int xa, int ya, int xb, int yb) {
+	return sqrt(pow((xa - xb), 2) + pow((ya - yb), 2));
 }
 
 int main() {
-	ios::sync_with_stdio(0);
 	cin.tie(0);
+	ios::sync_with_stdio(0);
 
-	int x_A, y_A, x_B, y_B, x_C, y_C;
-	cin >> x_A >> y_A >> x_B >> y_B >> x_C >> y_C;
-	Point A(x_A, y_A), B(x_B, y_B), C(x_C, y_C);
-	
-	double a, b, c;
-	a = get_length(B, C);
-	b = get_length(C, A);
-	c = get_length(A, B);
-	
-	double maximum = max(max(a, b), c);
-	double minimum = min(min(a, b), c);
-	double result;
-	if (line(A, B, C)) {
-		result = -1.0;
+	int xa, ya, xb, yb, xc, yc;
+	cin >> xa >> ya >> xb >> yb >> xc >> yc;
+
+	if (((xa-xb)*(ya-yc)) == ((ya-yb)*(xa-xc))) {
+		cout << -1.0;
+		return 0;
 	}
-	else {
-		result = 2 * (maximum - minimum);
-	}
-	cout << fixed << setprecision(12) << result;
+
+	double s1 = l2_norm(xa, ya, xb, yb) + l2_norm(xb, yb, xc, yc);
+	double s2 = l2_norm(xb, yb, xc, yc) + l2_norm(xc, yc, xa, ya);
+	double s3 = l2_norm(xc, yc, xa, ya) + l2_norm(xa, ya, xb, yb);
+
+	vector<double> v = { s1, s2, s3 };
+	double gap = *max_element(v.begin(), v.end()) - *min_element(v.begin(), v.end());
+	
+	cout << fixed;
+	cout.precision(9);
+	cout << 2 * gap;
 }
