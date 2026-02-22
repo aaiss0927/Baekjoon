2434137bf6f5e7be62828ae19a20f539f13eebb8
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-02-22 20:00:34 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,40 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 using namespace std;
 
-long long a, b;
-queue<pair<long long, int>> q;
-bool is_printed = false;
+int a, b;
+bool flag;
 
-void bfs() {
-    while (!q.empty()) {
-        pair<long long, int> cur = q.front();
-        q.pop();
-
-        if (cur.first == b) {
-            cout << cur.second + 1;
-            is_printed = true;
-            return;
-        }
-
-        if (cur.first * 2 <= b)
-            q.push({ cur.first * 2, cur.second + 1 });
-
-        if (cur.first * 10 + 1 <= b) 
-            q.push({ cur.first * 10 + 1, cur.second + 1 });
+void bt(int b, int cnt) {
+    if ((b == a) && !flag) {
+        cout << cnt;
+        flag = true;
+        return;
     }
+    
+    if (b < a) return;
+    
+    if (b % 2 == 0)
+        bt(b / 2, cnt + 1);
+    
+    if (b % 10 == 1)
+        bt(b / 10, cnt + 1);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
 
     cin >> a >> b;
-    q.push({a, 0});
-    bfs();
-
-    if (!is_printed)
-        cout << -1;
+    bt(b, 1);
+    
+    if (!flag) cout << -1;
 }
