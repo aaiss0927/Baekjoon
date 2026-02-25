69e63d59f26ac9f249620a939a84bcafe2714194
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-02-25 21:33:16 +0900

    [Silver I] Title: 카잉 달력, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.cc
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -1,54 +1,49 @@
 #include <iostream>
-#include <cmath>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-int GCD(int a, int b) {
-	if (b == 0) {
-		return a;
-	}
-	return GCD(b, a % b);
-}
-
-int LCM(int a, int b) {
-	return a * b / GCD(a, b);
+int gcd(int a, int b) {
+    if (a > b) swap(a, b);
+    
+    if (a == 0) return b;
+    
+    return gcd(b % a, a);
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int T; cin >> T;
-	while (T--) {
-		int M, N, x, y;
-		cin >> M >> N >> x >> y;
-		
-		int a, b;
-		a = b = 0;
-
-		if (M == 1 && N == 1) {
-			cout << "1\n";
-			continue;
-		}
-
-		bool flag = false;
-
-		while (M * a + x <= LCM(M, N)) {
-			if (M * a + x > N * b + y) {
-				b++;
-			}
-			else if (M * a + x < N * b + y) {
-				a++;
-			}
-			else {
-				cout << M * a + x << '\n';
-				flag = true;
-				break;
-			}
-		}
-		if (!flag) {
-			cout << "-1\n";
-		}
-	}
+    int T; cin >> T;
+    
+    while (T--) {
+        int m, n, x, y; cin >> m >> n >> x >> y;
+        int lcm = m * n / gcd(m, n);
+        bool flag = false;
+        
+        int a, b;
+        a = b = 0;
+        
+        while (m * a + x <= lcm) {
+            if (m * a + x < n * b + y) a++;
+            else if (m * a + x > n * b + y) b++;
+            else {
+                flag = true;
+                cout << m * a + x << '\n';
+                break;
+            }
+        }
+        
+        if (!flag) cout << "-1\n";
+    }
 }
+
+// <3:11> - 23
+// 23 = 10a + 3
+// 23 = 12b + 11
+// 23 = 60c + ()
+// 13 23 33 43 53
+// 11 23 35 47 59
+
+// maxi 내에서
+// mi + x vector
+// nj + y vector
+// 만들고 공통되는 1개 원소 찾기
+// 없다면 -1 출력
