bd5e459a3d7b823cbe25e2efd8c75690f3193f97
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-02-27 18:43:37 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 296 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.cc
@@ -1,22 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int T;
-	cin >> T;
-	int N;
-	while (T--) {
-		cin >> N;
-		vector<int> arr;
-		int num;
-		while (N--) {
-			cin >> num;
-			arr.push_back(num);
-		}
-		cout << *min_element(arr.begin(), arr.end()) << ' '
-			<< *max_element(arr.begin(), arr.end()) << '\n';
-	}
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int T; cin >> T;
+    
+    while (T--) {
+        int n; cin >> n;
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        
+        for (int i = 0; i < n; i++) {
+            int num; cin >> num;
+            
+            if (num > maxi) maxi = num;
+            if (num < mini) mini = num;
+        }
+        
+        cout << mini << ' ' << maxi << '\n';
+    }
 }
