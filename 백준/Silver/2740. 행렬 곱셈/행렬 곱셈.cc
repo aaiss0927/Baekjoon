d02fabdb067d792ab9989be3ef1c5c6d050b56cc
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-02-27 18:55:14 +0900

    [Silver V] Title: 행렬 곱셈, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int n, m, k; 
+    
+    cin >> n >> m;
+    vector<vector<int>> A(n, vector<int>(m));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            cin >> A[i][j];
+    }
+    
+    cin >> m >> k;
+    vector<vector<int>> B(m, vector<int>(k));
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < k; j++)
+            cin >> B[i][j];
+    }
+    
+    vector<vector<int>> C(n, vector<int>(k));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            for (int l = 0; l < m; l++)
+                C[i][j] += A[i][l] * B[l][j];
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++)
+            cout << C[i][j] << ' ';
+        cout << '\n';
+    }
+}
