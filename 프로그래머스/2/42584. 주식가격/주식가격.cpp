d233b56ed2801bcc62972edc3c97ab723202cc13
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-03-02 22:32:37 +0900

    [level 2] Title: 주식가격, Time: 27.61 ms, Memory: 24.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
-#include <string>
 #include <vector>
 #include <stack>
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     int n = prices.size();
-    vector<int> answer(n, 0);
-    stack<int> s; // 인덱스를 저장하는 스택
-    
+    vector<int> answer(n);
+    stack<int> s; // 가격이 떨어지지 않은 시점의 '인덱스'를 저장
+
     for (int i = 0; i < n; i++) {
+        // 하락장을 만났을 때 (현재 가격 < 스택 TOP 가격)
         while (!s.empty() && prices[s.top()] > prices[i]) {
-            int idx = s.top();
+            int top_idx = s.top();
             s.pop();
-            answer[idx] = i - idx;
+            answer[top_idx] = i - top_idx; // 버틴 시간 기록
         }
-        s.push(i);
+        s.push(i); // 현재 시점 저장
     }
-    
+
+    // 끝까지 가격이 떨어지지 않은 인덱스들 처리
     while (!s.empty()) {
-        int idx = s.top();
+        int top_idx = s.top();
         s.pop();
-        answer[idx] = n - 1 - idx;
+        answer[top_idx] = n - 1 - top_idx;
     }
-    
+
     return answer;
 }
