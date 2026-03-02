d5e016ef04596616dcc09ee78f79a66e538ea35b
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-03-02 15:35:38 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,39 +1,32 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <queue>
 #include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     int n = progresses.size();
     vector<int> answer;
-    vector<int> rests;
+    queue<int> rests;
     
-    for (int i = 0; i < n; i++) {
-        double temp = (100 - progresses[i]) / double(speeds[i]);
-        int rest = (temp > int(temp)) ? int(temp) + 1 : int(temp);
+    for (int i = 0; i < n; i++)
+        rests.push(int(ceil((100.0 - progresses[i]) / speeds[i])));
+    
+    while (!rests.empty()) {
+        int val = rests.front(); rests.pop();
+        int cnt = 1;
         
-        rests.push_back(rest);
-    }
-    
-    int cur = 1;
-    int cnt = 1;
-    for (int i = 0; i < n && cur < n;) {
-        while ((cur < n) && (rests[i] >= rests[cur])) {
+        while (!rests.empty() && val >= rests.front()) {
             cnt++;
-            cur++;
+            rests.pop();
         }
         
         answer.push_back(cnt);
-        cnt = 1;
-        i = cur;
-        cur++;
-        
-        if (i < n && cur >= n) answer.push_back(cnt);
     }
     
     return answer;
 }
 
-// 7 3 9 3   2 2
-// 15 10 1 1 20 1    4 2
+// 7 3 9   2 1
+// 15 10 1 1 20 1    1 3 2
