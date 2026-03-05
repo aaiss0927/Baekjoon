f8c1f5ef6f195bb5b8d8b2b9dadfa71aec463664
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-03-05 19:40:59 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.18 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -4,82 +4,51 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<bool>> graph;
+vector<vector<int>> graph;
 vector<bool> is_visited;
-int cnt, cnt1, cnt2;
-int answer = INT_MAX;
+int cnt;
 
 void setup(int n) {
     is_visited = vector<bool> (n + 1, false);
 }
 
-void dfs(int n, int num) {    
-    for (int i = 1; i <= n; i++) {
-        if (!is_visited[i] && graph[num][i]) {
-            is_visited[i] = true;
-            dfs(n, i);
-            cnt++;
-        }
+void dfs(int num, int v1, int v2) {
+    for (int i = 0; i < graph[num].size(); i++) {
+        int next_num = graph[num][i];
+        
+        if (is_visited[next_num]) continue;
+        if ((num == v1 && next_num == v2) || (num == v2 && next_num == v1)) continue;
+        
+        cnt++;
+        is_visited[next_num] = true;
+        dfs(next_num, v1, v2);
     }
-}
-
-void solve(int n) {
-    setup(n);
-    cnt = 1;
-    bool flag = false;
-    
-    for (int i = 1; i <= n; i++) {
-        if (is_visited[i]) continue;
-        
-        // cout << "노드" << i << "진입 ";
-        is_visited[i] = true;
-        cnt = 1;
-        
-        if (!flag) {
-            dfs(n, i);
-            cnt1 = cnt;
-        } 
-        else {
-            dfs(n, i);
-            cnt2 = cnt;
-        }
-        
-        flag = true;
-    }
-    // cout << "cnt1: " << cnt1 << " cnt2: " << cnt2;
-    
-    int temp = abs(cnt1 - cnt2);
-    if (temp < answer) answer = temp;
 }
 
 int solution(int n, vector<vector<int>> wires) {
-    graph = vector<vector<bool>> (n + 1, vector<bool>(n + 1));
+    int answer = INT_MAX;
+    graph = vector<vector<int>> (n + 1);
     
-    for (const auto& w : wires)
-        graph[w[0]][w[1]] = graph[w[1]][w[0]] = true;
+    for (const auto& w : wires) { // 인접 리스트 그래프 형성
+        graph[w[0]].push_back(w[1]);
+        graph[w[1]].push_back(w[0]);
+    }
     
     int hehe = 0;
-    for (const auto& w : wires) {
-        graph[w[0]][w[1]] = graph[w[1]][w[0]] = false;
-        // cout << "wires[" << hehe << "] 삭제: "; 
-        solve(n);
-        // cout << '\n';
-        graph[w[0]][w[1]] = graph[w[1]][w[0]] = true;
+    for (const auto& w : wires) { // wire 하나씩 끊고 트리 원소 세기 => cnt, n - cnt
+        setup(n);
+        is_visited[1] = true;
+        cnt = 1;
+        dfs(1, w[0], w[1]);
+        if (abs(n - 2*cnt) < answer) answer = abs(n - 2*cnt);
         hehe++;
     }
     
+    // for (int i = 1; i <= n; i++) { // 인접 리스트 그래프 출력
+    //     for (int j = 0; j < graph[i].size(); j++)
+    //         cout << graph[i][j] << ' ';
+    //     cout << '\n';
+    // }
+    
     return answer;
 }
-
-// 1
-//
-// 2
-// 3
-// 4
-
-// 2 <= n <= 100
-// wires.size() = n - 1
-// wires = [[v1, v2], ,,, ,]  v1-v2 연결됨
-// 1 <= v1 < v2 <= n
-// result: 두 트리 내 노드 개수 차이 절대값
-// 하나씩 끊고 두 트리에서 각각 dfs로 노드 개수 확인
