897349530b277847d88863ade8e3afef8c395267
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-03-05 15:12:01 +0900

    [level 2] Title: 소수 찾기, Time: 62.69 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,56 +1,73 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <set>
+#include <iostream>
 using namespace std;
 
-vector<bool> is_prime(10000000, true);
+int cnt = 0;
+bool is_not_prime[10000000];
 
-void check_prime() {
-    is_prime[0] = is_prime[1] = false;
+void chae() {
+    is_not_prime[0] = is_not_prime[1] = true;
     
-    for (int i = 2; i * i <= 10000000; i++) {
-        if (is_prime[i] == false) continue;
-        for (int j = i * 2; j <= 10000000; j += i) is_prime[j] = false;
+    for (int i = 2; i < 10000; i++) {
+        if (is_not_prime[i]) continue;
+        
+        for (int j = 2 * i; j < 10000000; j += i)
+            is_not_prime[j] = true;
     }
 }
+// 1  2  3  4  5  6  7  8  9  10
+// 11 12 13 14 15 16 17 18 19 20
+// 소수 아닌 것 체크
+// 초기 is_not_prime은 모두 false <=> 일단 모두 소수로 체크 됨
 
 vector<char> v;
-vector<bool> visited(7, false);
-unordered_set<int> cand;
+set<int> s;
+bool is_used[7];
 
-void bt(int n, string numbers) {
-    if (v.size() == n) {
-        string s = "";
-        for (char c : v) s += c;
-        int s_int = stoi(s);
-        cand.insert(s_int);
+void bt(const string& numbers, int target_len) {    
+    if (v.size() == target_len) {
+        string temp = "";
+        
+        for (const auto& v_ : v)
+            temp += v_;
+        
+        int result = stoi(temp);
+            
+        if (!is_not_prime[result] && s.find(result) == s.end()) {
+            s.insert(result);
+            cnt++;
+        }
+        
         return;
     }
     
-    for (int i = 0; i < numbers.size(); i++) {
-        if (visited[i]) continue;
+    for (int i = 0; i < numbers.length(); i++) {
+        if (is_used[i]) continue;
         
-        visited[i] = true;
         v.push_back(numbers[i]);
-        bt(n, numbers);
+        is_used[i] = true;
+        bt(numbers, target_len);
+        is_used[i] = false;
         v.pop_back();
-        visited[i] = false;
     }
 }
-
+// numbers를 벡터로 쪼갬 => {0, 1, 1}
+// v에 numbers[0 ~ numbers.size() - 1] 넣었다 빼기
+// v.size() == numbers.size()면 return
 int solution(string numbers) {
-    int answer = 0;
-    check_prime();
-    for (int i = 1; i <= numbers.length(); i++) bt(i, numbers);
-    for (auto it = cand.begin(); it != cand.end(); it++) if (is_prime[*it]) answer++;
-    return answer;
+    chae();
+    
+    for (int i = 1; i <= numbers.length(); i++)
+        bt(numbers, i);
+    
+    return cnt;
 }
 
-// 1 <= n <= 7
-// 실제 수 0 <= 9999999(10^7)
-// 소수 판별
-// 주어진 숫자 1개 ~ n개로 perm: 백트래킹
+// 1 <= numbers.size() <= 7
+// 0 <= numbers[i] <= 9
+// 0 <= final_num < 10^7
 
-//  1  2  3  4  5  6  7  8  9 10
-// 11 12 13 14 15 16 17 18 19 20
+// _______
+// 모든 경우의 수: 9 * 10^6 ~= 10^7 => 백트래킹 가능
