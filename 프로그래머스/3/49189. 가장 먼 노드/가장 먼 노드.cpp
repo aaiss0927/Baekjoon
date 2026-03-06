7b58841e37c2449e48a8098297159b54082db379
조현준 <101855229+aaiss0927@users.noreply.github.com>
2026-03-07 02:16:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 8.74 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,51 +1,56 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-vector<vector<int>> g;
+vector<vector<int>> graph;
 vector<int> dist;
-vector<bool> is_visited;
 queue<int> q;
-int maxi;
+int maxi = -1;
 
 void bfs() {
-    while(!q.empty()) {
+    while (!q.empty()) {
         int cur = q.front(); q.pop();
         
-        for (auto it = g[cur].begin(); it != g[cur].end(); it++) {
-            int next = *it;
+        for (const auto& g : graph[cur]) {
+            int next = g;
+            if (dist[next] != -1) continue;
             
-            if (!is_visited[next]) {
-                is_visited[next] = true;
-                dist[next] = dist[cur] + 1;
-                q.push(next);
-                if (maxi < dist[next]) maxi = dist[next];
-            }
+            dist[next] = dist[cur] + 1;
+            if (dist[next] > maxi) maxi = dist[next];
+            q.push(next);
         }
     }
 }
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
-    g = vector<vector<int>>(n + 1, vector<int>());
-    dist = vector<int>(n + 1, 0);
-    is_visited = vector<bool>(n + 1, false);
-    maxi = 0;
+    graph = vector<vector<int>> (n + 1);
+    dist = vector<int> (n + 1, -1);
     
-    for (auto it = edge.begin(); it != edge.end(); it++) {
-        int first = (*it)[0];
-        int second = (*it)[1];
-        g[first].push_back(second);
-        g[second].push_back(first);
+    for (const auto& e : edge) {
+        graph[e[0]].push_back(e[1]);
+        graph[e[1]].push_back(e[0]);
     }
     
+    dist[1] = 0;
     q.push(1);
-    is_visited[1] = true;
     bfs();
     
-    for (int i = 2; i <= n; i++)
+    for (int i = 1; i <= n ; i++) {
         if (dist[i] == maxi) answer++;
-
+    }
+    
+    // for (int i = 1; i <= n ; i++) {
+    //     for (int j = 0; j < graph[i].size(); j++)
+    //         cout << graph[i][j] << ' ';
+    //     cout << '\n';
+    // }
+    
     return answer;
 }
+
+// 2 <= v <= 20000
+// 1 <= e <= 50000
